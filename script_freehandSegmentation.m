985054bea7f71e7ca70dcbd2e74614fde4af674f
JOSE ALONSO SOLIS-LEMUS <Jose.Solis-Lemus@city.ac.uk>
2015-08-07 18:31:56 +0100

    Full Manual Segmentation demo.

--- script_freehandSegmentation.m
+++ script_freehandSegmentation.m
@@ -1,15 +1,12 @@
 % Script file:
-% Idea taken from:
+% Original demo with more features at:
 % http://www.mathworks.com/matlabcentral/...
 %           answers/32601-manual-segmentation-of-image
 %
 % Reworked by Jose Alonso Solis Lemus.
 %
 % Demo to have the user freehand draw an irregular shape over
-% a gray scale image, have it extract only that part to a new image,
-% and to calculate the mean intensity value of the image within that shape.
-% Also calculates the perimeter, centroid, and center of mass 
-% (weighted centroid).
+% a gray scale image.
 % 
 
 clear all
@@ -22,49 +19,68 @@ fontSize = 16;
 % Read a file
 [dataIn, att] = readParseInput();
 
-% Right now, we're only choosing the first image
-grayImage = dataIn(:,:,:,1);
+disp('Dataset attributes:');
+disp(att);
 
-% is it really gray?
-if att.Depth_RGB >3 
-    % If the image is 3D, we're segmenting the MEAN only. 
-    % This feature will be changed in future versions.
-    grayImage = mean(grayImage,3);
-elseif att.Depth_RGB == 3
-    grayImage = rgb2gray(grayImage);
+numImages = att.numImages;
+if numImages > 3
+    str = strcat('There are ', num2str(numImages), ...
+        'how many do you want to segment? [Default=92]: ');
+    a = input(str);
+    
+    if ~isempty(a)
+        numImages = a;
+    end
 end
 
-imagesc(grayImage);
-axis on;
-title('Original Grayscale Image', 'FontSize', fontSize);
-set(gcf, 'Position', get(0,'Screensize')); % Maximize figure.
+% output file
+dataBin = zeros(att.Height, att.Width, att.Depth_RGB, numImages);
 
-% Ask for the number of objects to be segmented.
-numCells = inputdlg('How many objects are we segmenting?');
-numCells = str2num(numCells{1});
+% deal with Warinings.
+set(0,'recursionlimit',750);
 
-if isempty(numCells)
-    uiwait(msgbox('Incorrect input, using numCells=1'));
-    numCells = 1;
+for i=1:numImages
+    for j=1:att.Depth_RGB
+        grayImage = dataIn(:,:,j,i);
+
+        imagesc(grayImage);
+        axis on;
+        
+        str = strcat('Original Grayscale Image: ',num2str(i), ...
+            ' Layer: ', num2str(j));
+        title(str , 'FontSize', fontSize);
+        set(gcf, 'Position', get(0,'Screensize')); % Maximize figure.
+        
+        % Ask for the number of objects to be segmented.
+        numCells = inputdlg('How many objects are we segmenting?',...
+            'Choose a number');
+        numCells = str2num(numCells{1});
+        
+        if isempty(numCells)
+            uiwait(msgbox('Incorrect input, using numCells=1'));
+            numCells = 1;
+        end
+        
+        %
+        message = sprintf(['Left click and hold to begin drawing.' ...
+            '\nSimply lift the mouse button to finish']);
+        uiwait(msgbox(message));
+        binaryImage = zeros(size(grayImage));
+        binaryImage = binaryImage > 5;
+        binaryImageSum = binaryImage;
+        
+        for k=1:numCells
+            hFH = imfreehand();
+            % Create a binary image ("mask") from the ROI object.
+            binaryImage = hFH.createMask();
+            xy = hFH.getPosition;
+            %
+            binaryImageSum = bitor(binaryImageSum,binaryImage);
+        end
+        
+        dataBin(:,:,j,i) = binaryImageSum;
+    end
 end
 
-%
-message = sprintf(['Left click and hold to begin drawing.' ...
-    '\nSimply lift the mouse button to finish']);
-uiwait(msgbox(message));
-binaryImage = zeros(size(grayImage));
-binaryImage = binaryImage > 5;
-binaryImageSum = binaryImage;
-
-for i=1:numCells
-    hFH = imfreehand();
-    % Create a binary image ("mask") from the ROI object.
-    binaryImage = hFH.createMask();
-    xy = hFH.getPosition;
-    %
-    binaryImageSum = bitor(binaryImageSum,binaryImage);
-end
-
-labeledImage = bwlabeln(binaryImageSum);
-imagesc(labeledImage);
+dataL = bwlabeln(dataBin);
 
